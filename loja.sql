8d272d2c25bff436a817ee65d3a217c900ae32ba
Alex Appelt <alexappelt6501@gmail.com>
2019-11-10 23:07:32 -0300

    inserção do arquivo de banco de dados

---
+++ loja.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 11-Nov-2019 às 03:06
+-- Versão do servidor: 10.4.6-MariaDB
+-- versão do PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `loja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `codigo` int(11) NOT NULL,
+  `descricao` varchar(30) NOT NULL,
+  `marca` varchar(30) NOT NULL,
+  `departamento` text NOT NULL,
+  `preco` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `imagem` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`codigo`, `descricao`, `marca`, `departamento`, `preco`, `quantidade`, `imagem`) VALUES
+(1, 'Camiseta Polo', 'Adidas', 'Social', 124, 4, 'f-p-2.jpg'),
+(2, 'Kelver viadao', 'Adidas', 'Social', 2424, 24, '/Applications/XAMPP/xamppfiles/htdocs/ecommerce/img/Classificacao de casos de uso.jpeg'),
+(3, 'Alex Lindo', 'Adidas', 'Social', 100, 1, 'Glorious Vihelmo-Rottis.png');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
