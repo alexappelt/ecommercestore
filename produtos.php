050846cc9d9bb8e5cb39c0327f427136e9b9cdb9
Alex Appelt <alexappelt6501@gmail.com>
2019-11-11 00:44:31 -0300

    ajuste de listar produtos na tela correta

--- produtos.php
+++ produtos.php
@@ -41,6 +41,53 @@ require'conexao.php';
 <button class="btn btn-info float-right"><a href="login.php">Cadastrar produtos</a></button>
 <button class="btn btn-warning float-rigt"><a href="listarprodutos.php">Lista de produtos</a></button>
 
+<table class="table table-striped">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Codigo</th>
+      <th scope="col">Descrição</th>
+      <th scope="col">Quantidade</th>
+      <th scope="col">Departamento</th>
+      <th scope="col">Preço</th>
+      <th scope="col">Marca</th>
+      <th scope="col">Foto</th>
+    </tr>
+  </thead>
+  
+    
+      <?php
+    require'conexao.php';
+    $sql = "select * from produto";
+    $busca = mysqli_query($conexao, $sql);
+    
+    while($array = mysqli_fetch_array($busca)){
+      $codigo = $array['codigo'];
+      $descricao = $array['descricao'];
+      $marca = $array['marca'];
+      $departamento = $array['departamento'];
+      $preco = $array['preco'];
+      $quantidade = $array['quantidade'];
+      $imagem = $array['imagem'];
+
+       ?>
+<tr>
+      <td> <?php echo $codigo  ?> </td>
+      <td> <?php echo $descricao  ?> </td>
+      <td> <?php echo $marca  ?> </td>
+      <td> <?php echo $departamento  ?> </td>
+      <td> <?php echo $preco  ?> </td>
+      <td> <?php echo $quantidade  ?> </td>
+      <td> <?php echo '<img width="150" src="img/'.$imagem.'"></img>' ?> </td>
+      
+
+<?php 
+}
+ ?>
+
+    </tr>
+
+</table>
+
 
 </body>
 </html>
