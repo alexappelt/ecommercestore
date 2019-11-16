2fdeb123d64296c42e749aa37219af9adb4d296f
Adrian <adrian.depra@unoesc.edu.br>
2019-11-15 22:24:14 -0300

    teste

--- action_cadastrar.php
+++ action_cadastrar.php
@@ -13,8 +13,6 @@ $imagem_nome = $_FILES['imagem']['name'];
 $location = 'img/'; 
 move_uploaded_file($_FILES['imagem']['tmp_name'], $location.$imagem_nome);
 
-//ola mundo
-
 
 $sql = "insert into produto (descricao, marca, departamento, preco, quantidade, imagem) values ('$descricao', '$marca', '$departamento', '$preco', '$quantidade' ,'$imagem_nome')";
 
